737c78cabdc60ccb48a6647b2a8f4bba129f02bd
Pabl01802 <58702798+Pabl01802@users.noreply.github.com>
2025-01-11 21:03:12 +0100

    chore: added husky

---
+++ commitlint.config.js
@@ -0,0 +1,33 @@
+const expectedTypes = [
+  "feat",
+  "fix",
+  "test",
+  "chore",
+  "build",
+  "refactor",
+  "docs",
+  "ci",
+]
+
+export default {
+  plugins: [
+    {
+      rules: {
+        "conventional-config": ({ type }) => {
+          if (!expectedTypes.includes(type)) {
+            return [
+              false,
+              `Type must be one of: ${expectedTypes.join(
+                ", "
+              )} \n Example: feat: add new feature`,
+            ]
+          }
+          return [true]
+        },
+      },
+    },
+  ],
+  rules: {
+    "conventional-config": [2, "always"],
+  },
+}
